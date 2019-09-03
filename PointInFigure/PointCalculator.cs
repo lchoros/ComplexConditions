35c3764bce030493d6f98b5e0e897d51224e5576
lchoros <choros777@gmail.com>
2019-09-03 13:44:56 +0100

    Find point in figure app

---
+++ PointInFigure/PointCalculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PointInFigure
+{
+    public class PointCalculator
+    {
+        public static void FindPointPlace(int h, int x, int y)
+        {
+            bool outRectangle1 = (x < 0 || x > 3 * h) || (y < 0 || y > h);
+            bool outRectangle2 = (x < h || x > 2 * h) || (y < h || y > 4 * h);
+
+            bool inRectangle1 = (x > 0 && x < h*3) && (y > 0 && y < h);
+            bool inRectangle2 = (x > h && x < 2 * h) && (y > h && y < 4 * h);
+
+            bool commmonBorder = (x > h && x < 2 * h) && y == h;
+
+            if (outRectangle1 && outRectangle2)
+            {
+                Console.WriteLine("outside");
+            }
+            else if (inRectangle1 || inRectangle2 || commmonBorder)
+            {
+                Console.WriteLine("inside");
+            }
+            else
+            {
+                Console.WriteLine("border");
+            }
+        }
+    }
+}
---
+++ PointInFigure/TestPointCalculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PointInFigure
+{
+    class TestPointCalculator
+    {
+        static void Main(string[] args)
+        {
+            PointCalculator.FindPointPlace(2, 3, 10);
+            PointCalculator.FindPointPlace(2, 3, 1);
+            PointCalculator.FindPointPlace(2, 2, 2);
+            PointCalculator.FindPointPlace(2, 6, 0);
+            PointCalculator.FindPointPlace(2, 0, 6);
+            PointCalculator.FindPointPlace(15, 13, 55);
+            PointCalculator.FindPointPlace(15, 29, 37);
+            PointCalculator.FindPointPlace(15, 37, 18);
+            PointCalculator.FindPointPlace(15, -4, 7);
+            PointCalculator.FindPointPlace(15, 30, 0);
+        }
+    }
+}
